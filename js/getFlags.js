c86b97556514f91676ccb0611a28db209c420844
Oli Taylor <oliver.0t.taylor@gmail.com>
2024-02-05 18:59:54 +0000

    Completed Congrats page and fixed Index counter

--- js/getFlags.js
+++ js/getFlags.js
@@ -6,11 +6,33 @@ function getFlags() {
 	const flagNum = 7; //This is to indicate the number of flags in the list + 1, for some reason using length+1 slows the page down massively
 	
 	for (let i = 0; i < flagNum; i++) {
-		if (flags[i] != null) {
+		if (flags[i] != null && flags[i] != 0) {
 			totalFlags += 1
 		}
 		
 	}
-	
 	document.getElementById('counter').innerHTML = totalFlags;
+
+return totalFlags;
+}
+
+function redirectPage() {
+	var flags = document.getElementById('counter').innerHTML;
+
+	if(window.location.href == 'test') {
+		window.location.href = "pages/flag"+flags+".html";
+	}
+
+	window.location.href = "pages/flag"+flags+".html";
+}
+
+function reset() {
+localStorage.setItem("1HHHGGfW2aBCH", 0);
+localStorage.setItem("2T2yXjXQEMb01", 0);
+localStorage.setItem("3LWyyxL40pZWp", 0);
+localStorage.setItem("4MXKQ1EZ0yv6A", 0);
+localStorage.setItem("5fcyR2QUg0ivi", 0);
+localStorage.setItem("6HFgA8TNxYFrb", 0);
+
+window.location.href = "../index.html"
 }
